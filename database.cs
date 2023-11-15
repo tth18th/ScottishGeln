c70f0182fe5365613a53c7724955d072c197b747
Tuhin <tuhin18th@gmail.com>
2023-11-15 16:13:57 +0000

    completed the employee window working every buttons

---
+++ database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace ScottishGeln
+{
+    public class database
+    {
+        private string connectionString = "server=lochnagar.abertay.ac.uk;username=sql2100258;password=reduces dump risk baths;database=sql2100258;";
+
+        //public void DatabaseManager(string connectionString)
+        //{
+        //    this.connectionString = connectionString;
+        //}
+
+        public MySqlConnection GetConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening connection: {ex.Message}");
+                connection.Close();
+                return null;
+            }
+        }
+    }
+}
